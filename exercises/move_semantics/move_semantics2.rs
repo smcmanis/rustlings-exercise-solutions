85d03f306f487caa284016c71ec69fa8bebe1ad1
smcmanis <simon.mcmanis@gmail.com>
2021-06-24 12:39:08 +1000

    Complete move_semantics module

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,12 +2,20 @@
 // Make me compile without changing line 13!
 // Execute `rustlings hint move_semantics2` for hints :)
 
-// I AM NOT DONE
 
 fn main() {
+    // // Solution 1: Deep copy with .clone()
     let vec0 = Vec::new();
+    let mut vec1 = fill_vec(vec0.clone()); // Solution 1: make deap copy of heap data
+    
+    // // Solution 2: borrow and copy
+    // let vec0 = Vec::new();
+    // let mut vec1 = fill_vec_borrow(&vec0); 
+    
+    // // Solution 3: Mutable borrow
+    // let mut vec0 = Vec::new();
+    // fill_vec_mut_borrow(&mut vec0); 
 
-    let mut vec1 = fill_vec(vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -25,4 +33,22 @@ fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     vec.push(66);
 
     vec
+}
+
+// Solution 2
+fn fill_vec_borrow(vec: &Vec<i32>) -> Vec<i32> {
+    let mut vec = vec.clone();
+
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
+
+    vec
+}
+
+// Solution 3
+fn fill_vec_mut_borrow(vec: &mut Vec<i32>) {
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
 }
