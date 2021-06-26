66eff6c824fb1c894f35dca84adddedebb7af19c
smcmanis <simon.mcmanis@gmail.com>
2021-06-26 16:51:11 +1000

    Complete enums module

--- enums/enums3.rs
+++ enums/enums3.rs
@@ -1,10 +1,11 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    Quit,
+    Echo(String),
+    Move(Point),
+    ChangeColor((u8, u8, u8)),
 }
 
 struct Point {
@@ -36,7 +37,18 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: create a match expression to process the different message variants
+        match message {
+            Message::Quit => {
+                self.quit = true;
+            }
+            Message::ChangeColor(color) => {
+                self.color = color;
+            }
+            Message::Echo(string) => {
+                println!("{}", string);
+            }
+            Message::Move(position) => self.position = position,
+        }
     }
 }
 
